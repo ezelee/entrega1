2c598f4ae1dba0a5ea6e868eb2e02e82da400713
floppylive <floppylive@hotmail.com>
2019-05-28 15:09:41 -0300

    patron strategy

---
+++ patron_strategy/patron_strategy/Pato.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace patron_strategy
+{
+    public interface IVolar
+    {
+        void Volar();
+    }
+
+    public interface IGraznar
+    {
+        void Graznar();
+    }
+    public class VolarConAlas : IVolar
+    {
+        public void Volar()
+        {
+            Console.WriteLine("Volando con alas");
+        }
+    }
+
+    public class NoVolar : IVolar
+    {
+        public void Volar()
+        {
+            Console.WriteLine("No puedo volar");
+        }
+    }
+    public class GraznarComoUnPato : IGraznar
+    {
+        public void Graznar()
+        {
+            Console.WriteLine("¡Cuac, cuac!");
+        }
+    }
+
+    public class GraznarComoUnaBocina : IGraznar
+    {
+        public void Graznar()
+        {
+            Console.WriteLine("¡Meec, meec!");
+        }
+    }
+
+    public class GraznarEnSilencio : IGraznar
+    {
+        public void Graznar()
+        {
+            Console.WriteLine("");
+        }
+    }
+    public class Pato
+    {
+        public readonly IGraznar _estrategiaGraznar;
+        public readonly IVolar _estrategiaVolar;
+
+
+        public Pato(IGraznar estrategiaGraznar, IVolar estrategiaVolar)
+        {
+            _estrategiaGraznar = estrategiaGraznar;
+            _estrategiaVolar = estrategiaVolar;
+        }
+
+
+
+        public void Graznar()
+        {
+            _estrategiaGraznar.Graznar();
+        }
+
+        public void Volar()
+        {
+            _estrategiaVolar.Volar();
+        }
+
+        public void Nadar()
+        {
+            Console.WriteLine("Nadando");
+        }
+    }
+    public class PatoDomestico : Pato
+    {
+        public PatoDomestico() :
+            base(new GraznarComoUnPato(), new VolarConAlas())
+        {
+        }
+
+
+    }
+
+    public class PatoDeJuguete : Pato
+    {
+
+
+        public PatoDeJuguete() :
+                base(new GraznarComoUnaBocina(), new NoVolar())
+        {
+        }
+
+
+
+    }
+
+    public class PatoDecorativo : Pato
+    {
+        public PatoDecorativo() :
+            base(new GraznarEnSilencio(), new NoVolar())
+        {
+
+        }
+
+
+
+
+    }
+}
---
+++ patron_strategy/patron_strategy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace patron_strategy
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            //Hay distintos tipos de patos que vuelan diferente y graznan diferente
+            //para no repetir codigo y poder intercambiar methods o comportamientos
+            //se utilizan clases "volar" y "graznar" y cada pato diferente
+            //usara una clase volar diferente
+            Console.WriteLine("Hello World!");
+            PatoDecorativo patito = new PatoDecorativo();
+            PatoDomestico patitodom = new PatoDomestico();
+
+            Console.Write("El patito decorativo vuela asi\n");
+            patito.Volar();
+
+            Console.Write("El patito domestico vuela asi\n");
+            patitodom.Volar();
+        }
+    }
+}
